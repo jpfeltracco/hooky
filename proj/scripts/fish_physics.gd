8c2b9ba4f2303f9370fde9b3efede7c6c64eb34b
Jeremy Feltracco <jpfeltracco@gatech.edu>
2017-09-04 22:43:32 -0400

    Basic up down.

--- proj/scripts/fish_physics.gd
+++ proj/scripts/fish_physics.gd
@@ -4,16 +4,34 @@ extends Node2D
 # var a = 2
 # var b = "textvar"
 
+var cur_pos = Vector2()
+
+const JUMP_VEL = 200
+const GRAVITY = 100
+
+var velocity = 0.0
+
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	self.set_process(true)
+	self.set_process_input(true)
 
 func _process(delta):
-	var cur_pos =  self.get_pos()
-	cur_pos.x += 100 * delta
+	cur_pos =  self.get_pos()
+	velocity -= GRAVITY * delta
 	
-	if (cur_pos.x > self.get_viewport_rect().size.width + self.get_item_rect().size.width/2):
-		cur_pos.x = -self.get_item_rect().size.width/2
-		
+	cur_pos.y -= velocity * delta
+	
 	self.set_pos(cur_pos)
+	
+	#if (cur_pos.x > self.get_viewport_rect().size.width + self.get_item_rect().size.width/2):
+	#	cur_pos.x = -self.get_item_rect().size.width/2
+
+	#self.set_pos(cur_pos)
+	
+func _input(event):
+	# if user left clicks
+	if event.type == InputEvent.MOUSE_BUTTON:
+		if event.button_index == 1 and velocity <= 0.0:
+			velocity = JUMP_VEL
